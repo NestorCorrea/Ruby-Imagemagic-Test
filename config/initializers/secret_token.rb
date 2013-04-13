1e9a8e832f0c42d1ae820f04c899b2a0b6fe0364
Nestor Correa <nestor@gelaskins.com>
2013-04-12 23:07:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyImagemagicTest::Application.config.secret_token = '02bd7e1ff2c009d3fc8b06d0f51cec57324e9d366ac27b9b66833dc52eb36bed016bc8be249e64e6897320ff49346d65f1965452ba72eeba8fc6fb1d39a2744c'
